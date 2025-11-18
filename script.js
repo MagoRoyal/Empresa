a4dc4b212a0b99333eed9741a4a1e09ee8ff6259
MagoRoyal <rodrigohalls2019@gmail.com>
2025-11-18 04:23:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Tema
+const toggle = document.getElementById("toggleTheme");
+toggle.onclick = () => document.body.classList.toggle("dark");
 
-const btn = document.getElementById("themeToggle");
-btn.onclick = () => document.body.classList.toggle("dark");
+// Formulário (protótipo funcional)
+document.getElementById("form").addEventListener("submit", function(e){
+    e.preventDefault();
+    document.getElementById("msg").innerText = "Formulário enviado! (Protótipo)";
+});
